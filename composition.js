eadfb515d9febbe54840d66a65e7f1e5d1685b9e
Willian <wsilvadev@gmail.com>
2019-08-11 22:53:57 -0300

    using composition with factories

--- composition.js
+++ composition.js
@@ -25,3 +25,47 @@ willian.andar(49);
 
 
 console.log(willian);
+//________-----------------_________________-____________-------------------___-___--__-__-________----_-______________---____--_-----_________--_-
+//################################################################################################################################################
+//---------------------------------------------------------------------------------------------------------
+const logavel = {
+    log(valor){
+        console.log(valor);
+    }
+}
+const emissorEvento = Object.assign({
+   emitir(event){
+    this.log(`evento: ${event}`);
+   }
+  
+}, logavel);
+ 
+function factorie(){
+  return  Object.assign({}, emissorEvento);
+}
+
+const conta = factorie();
+conta.emitir("evento log para qualquer arquivo!!!");
+// podemos importar  o factorie pra quaquer arquivo
+console.log(conta);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
